0d054aa404c6e19fb332588358ea151cc4b0a950
Ido <ido@gmail.com>
2026-03-17 18:44:08 +0200

    song

--- SpotiPSI/src/header/headerStyle.ts
+++ SpotiPSI/src/header/headerStyle.ts
@@ -1,11 +1,12 @@
 import { makeStyles } from "tss-react/mui";
 
-export const useStyles = makeStyles()(() => ({
-    container: {
-        background: "#383c3f"
+export const useStyles=makeStyles()(()=>({
+    container:{
+        background:"#383c3f"
     },
-    SpotiPSI: {
-        color: "purple",
+    SpotiPSI:{
+        color:"purple",
         padding: "20px"
     }
+    
 }));
